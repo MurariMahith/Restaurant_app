1409b6195bd98ace649021e9bcabdbdda60e5175
MurariMahith <murarimahith21@gmail.com>
2020-08-08 12:31:24 +0530

    template driven forms part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
@@ -51,13 +55,17 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    FormsModule,
     FlexLayoutModule,
+    MatToolbarModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule
   ],
   entryComponents: [
     LoginComponent
